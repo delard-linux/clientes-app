fed64f23013a526deeed6b26708a88af708897c9
David de la Rosa Delgado <drd_id@hotmail.com>
2022-01-02 00:55:34 +0100

    330. Cargar datos de cliente en formulario

--- src/app/clientes/form.component.ts
+++ src/app/clientes/form.component.ts
@@ -1,30 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
 import { ClienteService } from './cliente.service';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html'
 })
-export class FormComponent {
+export class FormComponent implements OnInit{
 
   cliente: Cliente = new Cliente();
   titulo: string = "Crear Cliente";
 
   constructor(private clienteService: ClienteService,
-    private router: Router){}
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+    ){}
 
-  public create(): void {
+  ngOnInit(): void {
+    this.cargarCliente();
+  }
+
+  create(): void {
     console.log(`Clicked! to create ${this.cliente.nombre}`);
     this.clienteService.create(this.cliente).subscribe(
       cliente => {
         this.router.navigate(['/clientes']);
         Swal.fire('nuevo cliente',`Cliente ${cliente.nombre} creado con éxito!`,'success');
-      }
-        );
-
+         });
   }
+
+  cargarCliente(): void {
+    this.activatedRoute.params.subscribe(
+      params => {
+        let id = params['id'];
+        if(id){
+          this.clienteService.getCliente(id).subscribe(
+            (cliente) => this.cliente = cliente);
+        }
+
+      });
+      console.log(`cargar cliente ${this.cliente.nombre}`);
+  }
+
+
 
 }
