6c989abc735a3970601196fd131f4b4cc9d8589e
David de la Rosa Delgado <drd_id@hotmail.com>
2022-01-01 10:32:32 +0100

    319. Conectando la app angular con el backend SpringBoot HttpClient

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -1,15 +1,21 @@
 import { Injectable } from '@angular/core';
 import { Cliente } from './cliente';
-import { CLIENTES } from './clientes.json';
-import { of, Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
 
+private urlEndPoint: string = 'http://localhost:8080/api/clientes';
+
+constructor(private http: HttpClient) {}
+
 getClientes(): Observable<Cliente[]> {
-  return of(CLIENTES);
+  return this.http.get(this.urlEndPoint).pipe(
+        map(response => response as Cliente[])
+      );
 }
 
 }
